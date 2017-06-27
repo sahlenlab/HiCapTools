0ebaf32d19a43cdd85041328c6d5b8da43ca763a
a_anil <aanil@kth.se>
2017-06-27 18:55:37 +0200

    Added scripts

---
+++ scripts/sortTranscriptFile.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#Sort Transcript file with header
+
+if [ -z "$1" ]; then
+	echo "Script to sort transcript file downloaded from UCSC to format required for HiCapTools"
+	echo "Usage: sh sortTranscriptFile.sh /path/to/file_name"
+	exit 1
+fi
+
+filename=$1
+namewoext="${filename%.*}"
+ext="${filename##*.}"
+
+sortedfilename=$namewoext
+sortedfilename="$sortedfilename.sorted."
+sortedfilename=$sortedfilename$ext
+
+
+(head -n 1 $filename && tail -n +2 $filename | sort -k13,13) > $sortedfilename
